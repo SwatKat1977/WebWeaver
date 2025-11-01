d7e545dd627825dab429f942e71a9ff917ab168c
SwatKat1977 <paulmorriss1977@gmail.com>
2025-11-01 03:35:08 +0000

    split up code

---
+++ visual_tester/preferences_dialog.py
@@ -0,0 +1,21 @@
+import wx
+
+
+class PreferencesDialog(wx.Dialog):
+    def __init__(self, parent, settings):
+        super().__init__(parent, title="Preferences", size=(320, 180))
+        vbox = wx.BoxSizer(wx.VERTICAL)
+
+        self.chk_snap = wx.CheckBox(self, label="Enable Grid Snapping")
+        self.chk_snap.SetValue(settings["snap_enabled"])
+
+        grid = wx.FlexGridSizer(1, 2, 8, 8)
+        grid.Add(wx.StaticText(self, label="Snap Size (px):"), 0, wx.ALIGN_CENTER_VERTICAL)
+        self.spin_snap = wx.SpinCtrl(self, min=5, max=200, initial=settings["snap_size"])
+        grid.Add(self.spin_snap, 1, wx.EXPAND)
+
+        vbox.Add(self.chk_snap, 0, wx.ALL, 10)
+        vbox.Add(grid, 0, wx.ALL | wx.EXPAND, 10)
+        vbox.Add(self.CreateSeparatedButtonSizer(wx.OK | wx.CANCEL), 0, wx.EXPAND | wx.ALL, 8)
+
+        self.SetSizer(vbox)
