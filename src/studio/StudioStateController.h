0e8f33e2a49c91d36e28ff9726e41644aa5bda3c
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2025-12-18 11:25:38 +0000

    state controller

---
+++ src/studio/StudioStateController.h
@@ -0,0 +1,58 @@
+/*
+This source file is part of Web Weaver
+For the latest info, see https://github.com/SwatKat1977/WebWeaver
+
+Copyright 2025 SwatKat1977
+
+    This program is free software : you can redistribute it and /or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see < https://www.gnu.org/licenses/>.
+*/
+
+#ifndef STUDIOSTATECONTROLLER_H_
+#define STUDIOSTATECONTROLLER_H_
+#include <functional>
+
+namespace webweaver::studio {
+
+enum class StudioState {
+    NoProject,
+    ProjectLoaded,
+    RecordingRunning,
+    RecordingPaused,
+    Inspecting
+};
+
+class StudioStateController {
+ public:
+    using StateChangedCallback = std::function<void(StudioState)>;
+
+    explicit StudioStateController(StateChangedCallback cb);
+
+    StudioState GetState() const;
+
+    // User intents
+    void OnProjectLoaded();
+    void OnRecordStartStop();
+    void OnRecordPause();
+    void OnInspectorToggle(bool shown);
+
+ private:
+    void SetState(StudioState newState);
+
+    StudioState state_ = StudioState::NoProject;
+    StateChangedCallback onStateChanged_;
+};
+
+}   // namespace webweaver::studio
+
+#endif  // STUDIOSTATECONTROLLER_H_
