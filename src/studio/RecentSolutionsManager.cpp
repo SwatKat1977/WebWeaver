f8aa30ce5783ff78560221256f3c9b99fd75be34
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2025-12-19 14:45:08 +0000

    implement recentsolutionsmanager

--- src/studio/RecentSolutionsManager.cpp
+++ src/studio/RecentSolutionsManager.cpp
@@ -17,11 +17,67 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.If not, see < https://www.gnu.org/licenses/>.
 */
+#include <fstream>
+#include <wx/stdpaths.h>
+#include <nlohmann/json.hpp>
 #include "RecentSolutionsManager.h"
 
 namespace webweaver::studio {
 
-class RecentSolutionsManager {
-};
+void RecentSolutionsManager::Load()
+{
+    recent_.clear();
+
+    std::ifstream in(GetStoragePath());
+    if (!in.is_open())
+        return;
+
+    nlohmann::json j;
+    in >> j;
+
+    if (!j.contains("recentSolutions"))
+        return;
+
+    for (const auto& entry : j["recentSolutions"]) {
+        recent_.emplace_back(entry.get<std::string>());
+    }
+}
+
+void RecentSolutionsManager::Save() const
+{
+    nlohmann::json j;
+    j["version"] = 1;
+
+    for (const auto& p : recent_)
+        j["recentSolutions"].push_back(p.string());
+
+    std::filesystem::create_directories(
+        GetStoragePath().parent_path());
+
+    std::ofstream out(GetStoragePath());
+    out << j.dump(2);
+}
+
+void RecentSolutionsManager::AddSolution(
+    const std::filesystem::path& path)
+{
+    auto it = std::find(recent_.begin(), recent_.end(), path);
+    if (it != recent_.end())
+        recent_.erase(it);
+
+    recent_.insert(recent_.begin(), path);
+
+    if (recent_.size() > kMaxRecent)
+        recent_.resize(kMaxRecent);
+
+    Save();
+}
+
+std::filesystem::path  RecentSolutionsManager::GetStoragePath() const {
+    wxString baseDir = wxStandardPaths::Get().GetUserConfigDir();
+    return std::filesystem::path(baseDir.ToStdString())
+        / "webweaver"
+        / "recent_solutions.json";
+}
 
 }   // namespace webweaver::studio
