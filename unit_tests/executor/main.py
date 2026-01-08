d3f3e6376f63d97b96dd667773b2abb2877cf613
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-01-08 20:09:10 +0000

    Class Resolver unit tests

--- unit_tests/executor/main.py
+++ unit_tests/executor/main.py
@@ -3,6 +3,7 @@ from test_test_suite_normalisation import TestNormaliseClasses, TestNormaliseSui
 from test_test_suite_suite_loader import TestTestSuiteSuiteLoader
 from test_test_suite_suite_validator import TestTestSuiteSuiteValidator
 from test_test_suite_suite_parser import TestTestSuiteSuiteParser
+from discovery.test_class_resolver import TestClassResolver
 
 if __name__ == "__main__":
     unittest.main()
