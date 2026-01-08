56451fd8fef7c4597cee7723d1e99ed884fd9a4d
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-01-08 20:41:09 +0000

    Test Preparation unit tests

--- unit_tests/executor/main.py
+++ unit_tests/executor/main.py
@@ -4,6 +4,7 @@ from test_test_suite_suite_loader import TestTestSuiteSuiteLoader
 from test_test_suite_suite_validator import TestTestSuiteSuiteValidator
 from test_test_suite_suite_parser import TestTestSuiteSuiteParser
 from discovery.test_class_resolver import TestClassResolver
+from discovery.test_test_preparation import TestTestPreparation
 
 if __name__ == "__main__":
     unittest.main()
