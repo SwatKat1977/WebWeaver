10f419dd6401ce0b3e2f20817b6e2018e3244685
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-01-13 22:16:56 +0000

    moved StudioMainFrame class to ui directory

--- studio/studio.py
+++ studio/studio.py
@@ -18,8 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import wx
-
-from studio_main_frame import StudioMainFrame
+from ui.studio_main_frame import StudioMainFrame
 
 
 class WebWeaverStudioApp(wx.App):
