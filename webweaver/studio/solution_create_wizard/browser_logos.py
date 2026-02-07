54c813846bfb71b16c66cbac7554cc455e6b33c6
SwatKat1977 <paulmorriss1977@gmail.com>
2026-02-07 21:29:53 +0000

    updated paths again...

--- webweaver/studio/solution_create_wizard/browser_logos.py
+++ webweaver/studio/solution_create_wizard/browser_logos.py
@@ -20,12 +20,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from io import BytesIO
 import wx
 # Import generated icon byte data
-from ..resources.browser_logos.browser_chromium_logo import BROWSER_CHROMIUM_LOGO
-from ..resources.browser_logos.browser_firefox_logo import BROWSER_FIREFOX_LOGO
-from ..resources.browser_logos.browser_google_chrome_logo import \
-    BROWSER_GOOGLE_CHROME_LOGO
-from ..resources.browser_logos.browser_microsoft_edge_logo import \
-    BROWSER_MICROSOFT_EDGE_LOGO
+from webweaver.studio.resources.browser_logos.browser_chromium_logo import \
+    BROWSER_CHROMIUM_LOGO
+from webweaver.studio.resources.browser_logos.browser_firefox_logo import \
+    BROWSER_FIREFOX_LOGO
+from webweaver.studio.resources.browser_logos.browser_google_chrome_logo \
+    import BROWSER_GOOGLE_CHROME_LOGO
+from webweaver.studio.resources.browser_logos.browser_microsoft_edge_logo \
+    import BROWSER_MICROSOFT_EDGE_LOGO
 
 
 def _load_logo(png_bytes: bytes) -> wx.Bitmap:
