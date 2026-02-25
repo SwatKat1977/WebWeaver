99883150872ada1715acb3ce21a26c033aae45f2
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-02-25 21:03:09 +0000

    minor fix

--- webweaver/common/assertion.py
+++ webweaver/common/assertion.py
@@ -1,14 +1,22 @@
 """
-Self-contained assertion utilities for WebWeaver.
+This source file is part of Web Weaver
+For the latest info, see https://github.com/SwatKat1977/WebWeaver
 
-Supports both hard (fail-fast) and soft (collecting) assertions.
+Copyright 2025-2026 SwatKat1977
 
-Designed to be framework-agnostic and reusable across:
-- Studio playback
-- Test executor
-- Future automation modules
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
-
 from __future__ import annotations
 from typing import Callable, Any
 import logging
