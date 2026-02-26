d463c452ffcc00a11d50406595da853b2c5fa4d1
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-02-26 12:00:47 +0000

    page layout

--- webweaver/studio/ui/framework/settings_dialog.py
+++ webweaver/studio/ui/framework/settings_dialog.py
@@ -72,10 +72,89 @@ class SettingsDialog(wx.Dialog):
         self._context = context
         self._page_definitions: typing.List[PageDefinition] = page_definitions
 
-        self._tree = wx.TreeCtrl(self)
-        self._content_area = wx.Panel(self)
-
         # Maps tree items to their corresponding SettingsPage instance.
         # Each page instance is created once and reused while the dialog
         # remains open.
         self._pages: dict[wx.TreeItemId, SettingsPage] = {}
+
+        self._create_ui()
+
+    def _create_ui(self):
+        self._tree = wx.TreeCtrl(self)
+        self._content_area = wx.Panel(self)
+
+        self._content_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._content_area.SetSizer(self._content_sizer)
+
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        body_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        body_sizer.Add(self._tree, 0, wx.EXPAND | wx.ALL, 5)
+        body_sizer.Add(self._content_area, 1, wx.EXPAND | wx.ALL, 5)
+
+        button_sizer = self.CreateSeparatedButtonSizer(
+            wx.OK | wx.CANCEL | wx.APPLY)
+
+        main_sizer.Add(body_sizer, 1, wx.EXPAND)
+        main_sizer.Add(button_sizer, 0, wx.EXPAND | wx.ALL, 5)
+
+        self.SetSizer(main_sizer)
+        self.SetSize((700, 500))
+
+        root = self._tree.AddRoot("Settings")
+
+        for definition in self._page_definitions:
+            item = self._tree.AppendItem(root, definition.label)
+
+            page = definition.page_class(self._content_area, self._context)
+            page.Hide()
+            page.load()
+
+            self._content_sizer.Add(page, 1, wx.EXPAND)
+
+            self._pages[item] = page
+
+        # Expand Root
+        self._tree.Expand(root)
+
+        # Select first item automatically
+        first_item, _ = self._tree.GetFirstChild(root)
+        if first_item.IsOk():
+            self._tree.SelectItem(first_item)
+            self._show_page(first_item)
+
+        self._tree.Bind(wx.EVT_TREE_SEL_CHANGED,
+                        self._on_tree_selection_changed)
+
+        self.Bind(wx.EVT_BUTTON, self._on_ok, id=wx.ID_OK)
+        self.Bind(wx.EVT_BUTTON, self._on_apply, id=wx.ID_APPLY)
+
+    def _show_page(self, item: wx.TreeItemId):
+        for page in self._pages.values():
+            page.Hide()
+
+        selected_page = self._pages.get(item)
+        if selected_page:
+            selected_page.Show()
+            self._content_area.Layout()
+
+    def _on_tree_selection_changed(self, event):
+        item = event.GetItem()
+        self._show_page(item)
+
+    def _apply_all(self) -> bool:
+        for page in self._pages.values():
+            if not page.validate():
+                return False
+
+        for page in self._pages.values():
+            page.apply()
+
+        return True
+
+    def _on_ok(self, event):
+        if self._apply_all():
+            self.EndModal(wx.ID_OK)
+
+    def _on_apply(self, event):
+        self._apply_all()
