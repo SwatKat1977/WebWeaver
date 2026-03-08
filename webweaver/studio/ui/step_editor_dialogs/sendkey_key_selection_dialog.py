5d0f8e7e7d67d84bb273c63f8a3841d3f0bc3dc4
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-03-08 23:57:12 +0000

    pylint update

--- webweaver/studio/ui/step_editor_dialogs/sendkey_key_selection_dialog.py
+++ webweaver/studio/ui/step_editor_dialogs/sendkey_key_selection_dialog.py
@@ -1,7 +1,46 @@
+"""
+This source file is part of Web Weaver
+For the latest info, see https://github.com/SwatKat1977/WebWeaver
+
+Copyright 2025-2026 SwatKat1977
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
 import wx
 
 
 class SendkeyKeySelectionDialog(wx.Dialog):
+    """Dialog that allows the user to select a keyboard key and modifiers.
+
+    This dialog is used when building a send-keys sequence. The user can
+    choose a special key from a predefined list and optionally apply
+    modifier keys such as CTRL, ALT, SHIFT, or META.
+
+    Attributes:
+        KEY_CHOICES (list[str]): List of available special keys that can be
+            selected.
+        key_choice (wx.Choice): Dropdown widget used to select the base key.
+        ctrl (wx.CheckBox): Checkbox indicating whether the CTRL modifier
+            should be applied.
+        alt (wx.CheckBox): Checkbox indicating whether the ALT modifier
+            should be applied.
+        shift (wx.CheckBox): Checkbox indicating whether the SHIFT modifier
+            should be applied.
+        meta (wx.CheckBox): Checkbox indicating whether the META modifier
+            should be applied.
+    """
+    # pylint: disable=too-few-public-methods
 
     KEY_CHOICES = [
         "ENTER", "TAB", "ESCAPE", "BACKSPACE", "DELETE",
@@ -11,7 +50,11 @@ class SendkeyKeySelectionDialog(wx.Dialog):
     ]
 
     def __init__(self, parent):
+        """Initializes the key selection dialog.
 
+        Args:
+            parent (wx.Window): Parent window that owns this dialog.
+        """
         super().__init__(parent, title="Select Key", size=(300, 250))
 
         panel = wx.Panel(self)
@@ -53,7 +96,17 @@ class SendkeyKeySelectionDialog(wx.Dialog):
         panel.SetSizer(main_sizer)
 
     def get_result(self):
+        """Returns the selected key and modifier combination.
 
+        The selected key is taken from the dropdown list, while the modifier
+        string is constructed from the selected checkboxes.
+
+        Returns:
+            tuple[str, Optional[str]]: A tuple containing:
+                - key: The selected key name.
+                - modifiers: A '+'-joined string of modifiers (e.g. "CTRL+SHIFT")
+                  or ``None`` if no modifiers were selected.
+        """
         key = self.key_choice.GetStringSelection()
 
         modifiers = []
