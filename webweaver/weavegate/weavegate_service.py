b6074841f240a436bd147d39b483ecdd686825a3
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-03-28 23:39:05 +0000

    Initial work on weavegate

--- webweaver/weavegate/weavegate_service.py
+++ webweaver/weavegate/weavegate_service.py
@@ -1,3 +1,22 @@
+"""
+This source file is part of Web Weaver
+For the latest info, see https://github.com/SwatKat1977/WebWeaver
+
+Copyright 2025-2026 SwatKat1977
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
 import asyncio
 import logging
 import os
@@ -138,18 +157,13 @@ class WeaveGateService:
         Returns:
             Boolean: True => Successful, False => Unsuccessful.
         """
-
-        '''
-SERVICE_COPYRIGHT_TEXT = 'Copyright 2025-2026 Webweaver Development Team'
-LICENSE_TEXT = 'Licensed under the GNU General Public License, Version 2.0'
-        '''
-
         self._logger.info("WeaveGate Service %s", gate_version)
         self._logger.info(("Part of Webweaver "
                            "(https://github.com/SwatKat1977/WebWeaver)"))
         self._logger.info("Copyright 2025-2026 Webweaver Development Team")
         self._logger.info(("Licensed under the GNU General Public License, "
                            "Version 2.0"))
+
         return True
 
     async def _main_loop(self) -> None:
